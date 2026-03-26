d4b80dce1ae13c67a1b3239af5e8a948ef63a57d
Jaydev Parmar <jydev@duck.com>
2026-03-26 16:08:41 +0530

    Add archive index, public API, misc

--- src/lib/types/database.d.ts
+++ src/lib/types/database.d.ts
@@ -4,114 +4,137 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export type Database = {
   // Allows to automatically instantiate createClient with right options
   // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
   __InternalSupabase: {
-    PostgrestVersion: "14.1";
-  };
+    PostgrestVersion: "14.1"
+  }
   public: {
     Tables: {
       APODs: {
         Row: {
-          copyright: string | null;
-          created_at: string | null;
-          date: string;
-          explanation: string | null;
-          hdurl: string | null;
-          media_type: string | null;
-          service_version: string | null;
-          thumbnail_url: string | null;
-          title: string | null;
-          url: string | null;
-        };
+          copyright: string | null
+          created_at: string | null
+          date: string
+          explanation: string | null
+          fts: unknown
+          hdurl: string | null
+          media_type: string | null
+          service_version: string | null
+          thumbnail_url: string | null
+          title: string | null
+          url: string | null
+        }
         Insert: {
-          copyright?: string | null;
-          created_at?: string | null;
-          date: string;
-          explanation?: string | null;
-          hdurl?: string | null;
-          media_type?: string | null;
-          service_version?: string | null;
-          thumbnail_url?: string | null;
-          title?: string | null;
-          url?: string | null;
-        };
+          copyright?: string | null
+          created_at?: string | null
+          date: string
+          explanation?: string | null
+          fts?: unknown
+          hdurl?: string | null
+          media_type?: string | null
+          service_version?: string | null
+          thumbnail_url?: string | null
+          title?: string | null
+          url?: string | null
+        }
         Update: {
-          copyright?: string | null;
-          created_at?: string | null;
-          date?: string;
-          explanation?: string | null;
-          hdurl?: string | null;
-          media_type?: string | null;
-          service_version?: string | null;
-          thumbnail_url?: string | null;
-          title?: string | null;
-          url?: string | null;
-        };
-        Relationships: [];
-      };
-    };
+          copyright?: string | null
+          created_at?: string | null
+          date?: string
+          explanation?: string | null
+          fts?: unknown
+          hdurl?: string | null
+          media_type?: string | null
+          service_version?: string | null
+          thumbnail_url?: string | null
+          title?: string | null
+          url?: string | null
+        }
+        Relationships: []
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
       get_apods_by_date: {
-        Args: { d: number; m: number };
+        Args: { d: number; m: number }
         Returns: {
-          copyright: string | null;
-          created_at: string | null;
-          date: string;
-          explanation: string | null;
-          hdurl: string | null;
-          media_type: string | null;
-          service_version: string | null;
-          thumbnail_url: string | null;
-          title: string | null;
-          url: string | null;
-        }[];
+          copyright: string | null
+          created_at: string | null
+          date: string
+          explanation: string | null
+          fts: unknown
+          hdurl: string | null
+          media_type: string | null
+          service_version: string | null
+          thumbnail_url: string | null
+          title: string | null
+          url: string | null
+        }[]
         SetofOptions: {
-          from: "*";
-          to: "APODs";
-          isOneToOne: false;
-          isSetofReturn: true;
-        };
-      };
-    };
+          from: "*"
+          to: "APODs"
+          isOneToOne: false
+          isSetofReturn: true
+        }
+      }
+      get_random_apod: {
+        Args: { n: number }
+        Returns: {
+          copyright: string | null
+          created_at: string | null
+          date: string
+          explanation: string | null
+          fts: unknown
+          hdurl: string | null
+          media_type: string | null
+          service_version: string | null
+          thumbnail_url: string | null
+          title: string | null
+          url: string | null
+        }[]
+        SetofOptions: {
+          from: "*"
+          to: "APODs"
+          isOneToOne: false
+          isSetofReturn: true
+        }
+      }
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
-};
+      [_ in never]: never
+    }
+  }
+}
 
-type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">;
+type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">
 
-type DefaultSchema = DatabaseWithoutInternals[Extract<
-  keyof Database,
-  "public"
->];
+type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "public">]
 
 export type Tables<
   DefaultSchemaTableNameOrOptions extends
     | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
     | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
         DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
       DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R;
+      Row: infer R
     }
     ? R
     : never
@@ -119,98 +142,98 @@ export type Tables<
         DefaultSchema["Views"])
     ? (DefaultSchema["Tables"] &
         DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
-        Row: infer R;
+        Row: infer R
       }
       ? R
       : never
-    : never;
+    : never
 
 export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I;
+      Insert: infer I
     }
     ? I
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
     ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Insert: infer I;
+        Insert: infer I
       }
       ? I
       : never
-    : never;
+    : never
 
 export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U;
+      Update: infer U
     }
     ? U
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
     ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Update: infer U;
+        Update: infer U
       }
       ? U
       : never
-    : never;
+    : never
 
 export type Enums<
   DefaultSchemaEnumNameOrOptions extends
     | keyof DefaultSchema["Enums"]
     | { schema: keyof DatabaseWithoutInternals },
   EnumName extends DefaultSchemaEnumNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
     : never = never,
 > = DefaultSchemaEnumNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
     ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
-    : never;
+    : never
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof DefaultSchema["CompositeTypes"]
     | { schema: keyof DatabaseWithoutInternals },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
     : never = never,
 > = PublicCompositeTypeNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
     ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never;
+    : never
 
 export const Constants = {
   public: {
     Enums: {},
   },
-} as const;
+} as const
